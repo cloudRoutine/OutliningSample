0fb97b977e3a69a0ddd46be71357ff1f3a9fa3d7
Jared Hester <jared.hester@gmail.com>
2015-10-13 04:26:10 -0400

    initial examples

---
+++ OutliningSample/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace OutliningSample.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("OutliningSample")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("OutliningSample")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("5eef3738-6a61-44bd-a48b-c9d6da4a246d")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ OutliningSample/Script.fsx
@@ -0,0 +1,239 @@
+﻿#r "System.Runtime"
+#r "System"
+#I @"..\."
+#load "Library1.fs"
+
+#load  "File1.fs"
+       "File2.fs"       
+       "File3.fs"
+
+
+
+
+open System
+open System.Collections
+open System.Collections.Generic
+
+
+let rec recfunc a = 
+    ()
+    ()
+
+and andfunc a =
+    ()
+    ()
+
+let outerfunc a =
+    let rec inner_recfunc a = 
+        ()
+        ()
+
+    and inner_andfunc a =
+        ()
+        ()
+    ()
+
+type rectype() =
+    member __.nada = ()
+
+and andtype() =
+    member __.nada = ()
+
+and andtype2() =
+    member __.nada = ()
+
+
+[   "item"
+    "item"
+    "item"  ] |> ignore
+
+[|  "item"
+    "item"
+
+    "item"  |] |> ignore
+
+let list_and_array = 
+ 
+    [   "item"
+        "item"
+        "item"  ] |> ignore
+
+    [|  "item"
+        "item"
+
+        "item"  |] |> ignore
+
+
+[<  CompilationRepresentationAttribute  
+    (CompilationRepresentationFlags.ModuleSuffix)
+>]
+module ``Who Cares?`` =
+    ()
+
+
+seq {
+        10  ..   1000
+    } |> ignore
+
+if true
+    && true || true
+    && true
+    then
+        ()
+        (
+        )
+        ()
+        (
+    
+    
+        )
+        ()
+
+elif 
+    true 
+ && true 
+ || true
+    then
+        ()  
+        ()
+        ()
+
+else 
+    ()
+    ()
+    ()
+
+type thenConstructor() =
+    
+    member __.Useless = 
+        ()
+
+    new(str:string) =
+        thenConstructor()
+
+        then   // should be an outline starting here
+            ()
+            ()
+            ()
+
+    new(x:int) =
+        thenConstructor(
+        
+        // constructors need outlining too you know :P
+        
+        
+            )
+
+
+{ new IEnumerable with
+
+    member __.GetEnumerator(): IEnumerator = 
+        ()        
+        ()
+        Unchecked.defaultof<_>
+}        
+
+type System.String with
+
+    static member DOES_NOTHING = 
+        ()
+        ()
+        "seriously nothing" |> ignore
+
+
+
+
+
+let matchfunction =
+    
+    function 
+    | 0 -> ()
+    | 1 -> ()
+    | 2 -> ()
+    | 3 -> ()
+    | 4 -> ()
+    | 5 -> ()
+    | _ -> ()
+   
+let matchwith num =
+
+    match num with
+    | 0 -> ()
+    | 1 -> ()
+    | 2 -> ()
+    | 3 -> ()
+    | 4 -> ()
+    | 5 -> ()
+    | _ -> ()
+
+
+( fun x -> 
+    ()
+    ()
+    ()  ) |> List.iter <| []
+    
+
+[ for x in 0 .. 100 -> 
+    ()
+    ()
+    x   ]
+
+
+for x = 100 downto 10 do
+    ()
+    ()
+    () |> ignore
+   
+
+// comblock
+// comblock
+// comblock
+
+/// docblock
+/// docblock
+/// docblock
+/// docblock
+
+(*
+    Ocaml comments
+    the best comments
+*)
+
+
+[<AbstractClass>]
+type abclass() =
+
+    abstract abmember : unit 
+            -> unit
+            -> unit
+            -> unit
+            -> unit
+
+    default __.abmember ()()()() =
+        ()
+        ()
+
+    override __.ToString()  =
+        ()
+        ()
+        ""
+
+let somef x =
+    ()
+    ()
+    ()
+    () in let somef2 y =
+                ()
+                ()
+                () in let somef3 z =
+                        ()
+                        ()
+                        ()
+                        () 
+
+
+
+
+
+
+()
